7771505b754eac7f692c7a6bc9e2fe915cfaf947
FrankFourierX <f.f.tandoi@gmail.com>
2024-07-11 15:20:19 +0200

    update

--- prisma/migrations/20240116102008_new_set_3/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sharesAddresses` on the `Strategy` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Strategy" DROP COLUMN "sharesAddresses",
-ADD COLUMN     "sharesAddress" TEXT;
--- src/web3/web3.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { Web3Service } from './web3.service';
-import { ApiService } from '../services/api.service'; // Adjust the import based on the actual location
-
-@Module({
-  providers: [Web3Service, ApiService],
-  exports: [Web3Service, ApiService],
-})
-export class Web3Module {}
